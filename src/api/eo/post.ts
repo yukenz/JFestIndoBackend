2e0fc87cf9c40aed12762da16296cc01f3ebeb45
Awan <yuyun.purniawan@gmail.com>
2023-07-21 20:14:36 +0700

    fix post

--- src/api/eo/post.ts
+++ src/api/eo/post.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import { HydratedDocument } from "mongoose"
 import { EO, IEO } from "../../models/eo"
 import { Event, IEvent } from "../../models/event";
+import handleError from "../../helpers/handleError";
 
 export default async function post(req: Request, res: Response) {
     const { email, username, nama, password } = req.body as IEO
@@ -10,6 +11,12 @@ export default async function post(req: Request, res: Response) {
         nama, email, username, password
     })
 
-    await eo.save().catch((err) => res.send(err.errors))
+    try {
 
+        await eo.save()
+        res.send("Sukses")
+
+    } catch (error) {
+        handleError(error, res)
+    }
 }
